29c1b500192a60c28ecfbc32f307ea9a50a48389
Lucas Figueroa <lucas_figueroa_11@hotmail.com>
2023-08-15 22:59:25 -0300

    agregamos todas las rutas al componente App

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,41 @@ import CrearProducto from "./components/views/producto/CrearProducto";
 import EditarProducto from "./components/views/producto/EditarProducto";
 import Administrador from "./components/views/Administrador";
 import Loguin from "./components/views/Login";
+import Registro from "./components/views/Registro";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu></Menu>
-      {/* <Error404></Error404> */}
-      {/* <Inicio></Inicio> */}
-      {/* <DetalleProducto></DetalleProducto> */}
-      {/* <CrearProducto></CrearProducto> */}
-      {/* <EditarProducto></EditarProducto> */}
-      {/* <Administrador></Administrador> */}
-      <Loguin></Loguin>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/loguin" element={<Loguin></Loguin>}></Route>
+        <Route exact path="/registro" element={<Registro></Registro>}></Route>
+        <Route
+          exact
+          path="/detalle"
+          element={<DetalleProducto></DetalleProducto>}
+        ></Route>
+        <Route
+          exact
+          path="/administrador"
+          element={<Administrador></Administrador>}
+        ></Route>
+        <Route
+          exact
+          path="/administrador/crear"
+          element={<CrearProducto></CrearProducto>}
+        ></Route>
+        <Route
+          exact
+          path="/administrador/editar"
+          element={<EditarProducto></EditarProducto>}
+        ></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
